fda2ef39784d2c86f339902cb8c1141491b99831
David Grieve <david.grieve@microsoft.com>
2021-08-24 10:01:26 -0400

    add duration to jvmevent tostring

--- core/api/src/main/java/com/microsoft/gctoolkit/event/jvm/JVMEvent.java
+++ core/api/src/main/java/com/microsoft/gctoolkit/event/jvm/JVMEvent.java
@@ -46,6 +46,7 @@ public abstract class JVMEvent {
             string.append(timeStamp.toString());
         else
             string.append("unknown");
+        string.append(':').append(getDuration());
         return string.toString();
     }
 
