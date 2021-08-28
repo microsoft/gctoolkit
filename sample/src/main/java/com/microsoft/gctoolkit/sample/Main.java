11d69c1ab987935e0ebb63f8fa62ff791a6d2457
Shubham Mishra <shubhammishrakv2@gmail.com>
2021-08-29 03:45:02 +0530

    Removing unused imports from sample/Main

--- sample/src/main/java/com/microsoft/gctoolkit/sample/Main.java
+++ sample/src/main/java/com/microsoft/gctoolkit/sample/Main.java
@@ -4,10 +4,8 @@ import com.microsoft.gctoolkit.GCToolKit;
 import com.microsoft.gctoolkit.io.GCLogFile;
 import com.microsoft.gctoolkit.io.SingleGCLogFile;
 import com.microsoft.gctoolkit.jvm.JavaVirtualMachine;
-import com.microsoft.gctoolkit.sample.aggregation.HeapOccupancyAfterCollectionAggregation;
 import com.microsoft.gctoolkit.sample.aggregation.HeapOccupancyAfterCollectionSummary;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Main {
@@ -15,7 +13,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         if (args.length == 0) {
             throw new IllegalArgumentException("This sample requires a path to a GC log file as an argument.");
-        }        
+        }
         Main main = new Main();
         main.execute(args[0]);
     }
@@ -26,7 +24,7 @@ public class Main {
         JavaVirtualMachine machine = gcToolKit.analyze(logFile);
         HeapOccupancyAfterCollectionSummary results = machine.getAggregation(HeapOccupancyAfterCollectionSummary.class);
         results.get().forEach((gcType, dataSet) ->
-            System.out.println("The XYDataSet for " + gcType + " contains " + dataSet.size() + " items.")
+                System.out.println("The XYDataSet for " + gcType + " contains " + dataSet.size() + " items.")
         );
     }
 }
