4a7ff5ca9d708e7c8f44f3b6449e51493fe0a9b7
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2021-08-28 10:14:27 +0300

    Added JavaDocs and changed parameter names to "unused"

--- core/api/src/main/java/com/microsoft/gctoolkit/util/concurrent/StartingGun.java
+++ core/api/src/main/java/com/microsoft/gctoolkit/util/concurrent/StartingGun.java
@@ -2,7 +2,21 @@ package com.microsoft.gctoolkit.util.concurrent;
 
 import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 
+/**
+ * A StartingGun for services, where we can wait for one particular service
+ * to start up.
+ * <p>
+ * Instead of {@link java.util.concurrent.CountDownLatch}, we have a custom
+ * concurrency utility called StartingGun, which is like a CountDownLatch with
+ * a count of 1 and where the awaitUninterruptibly() method does not throw an
+ * exception. Similarly to the CountDownLatch, this uses the
+ * {@link AbstractQueuedSynchronizer} for the synchronization.
+ *
+ * @author Dr Heinz M. Kabutz
+ */
 public class StartingGun {
+    private static final int UNUSED = 0;
+
     /**
      * Synchronization control For StartingGun.
      * Uses AQS state to represent count.
@@ -18,11 +32,11 @@ public class StartingGun {
             return getState();
         }
 
-        protected int tryAcquireShared(int acquires) {
+        protected int tryAcquireShared(int unused) {
             return (getState() == 0) ? 1 : -1;
         }
 
-        protected boolean tryReleaseShared(int releases) {
+        protected boolean tryReleaseShared(int unused) {
             // Decrement count; signal when transition to zero
             for (; ; ) {
                 int c = getState();
@@ -44,11 +58,18 @@ public class StartingGun {
         this.sync = new Sync(1);
     }
 
+    /**
+     * Wait for the starting gun to fire, without propagating the
+     * InterruptedException.
+     */
     public void awaitUninterruptibly() {
-        sync.acquireShared(1);
+        sync.acquireShared(UNUSED);
     }
 
+    /**
+     * Indicate that the service is ready for operation.
+     */
     public void ready() {
-        sync.releaseShared(1);
+        sync.releaseShared(UNUSED);
     }
 }
