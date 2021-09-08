a0927ae018ef22756ead2499645696d648236735
Bruno Borges <brborges@microsoft.com>
2021-09-08 18:47:58 +0000

    Adds comments to Main.java in sample

--- sample/src/main/java/com/microsoft/gctoolkit/sample/Main.java
+++ sample/src/main/java/com/microsoft/gctoolkit/sample/Main.java
@@ -1,6 +1,7 @@
 package com.microsoft.gctoolkit.sample;
 
 import com.microsoft.gctoolkit.GCToolKit;
+import com.microsoft.gctoolkit.io.GCLogFile;
 import com.microsoft.gctoolkit.io.SingleGCLogFile;
 import com.microsoft.gctoolkit.sample.aggregation.HeapOccupancyAfterCollectionSummary;
 
@@ -12,20 +13,35 @@ public class Main {
         if (args.length == 0) {
             throw new IllegalArgumentException("This sample requires a path to a GC log file as an argument.");
         }
-        var gcLogFilePath = args[0];
-
-        var logFile = new SingleGCLogFile(Path.of(gcLogFilePath));
         
-        var gcToolKit = new GCToolKit();
+        /**
+         * GC log files can come in  one of two types: single or series of rolling logs.
+         * In this sample, we load a single log file.
+         * The log files can be either in text, zip, or gzip format.
+         */
+        GCLogFile logFile = new SingleGCLogFile(Path.of(args[0]));
+        GCToolKit gcToolKit = new GCToolKit();
+
+        /**
+         * This call will load all implementations of Aggregator that have been declared in module-info.java.
+         * This mechanism makes use of Module SPI.
+         */
         gcToolKit.loadAggregationsFromServiceLoader();
 
-        var machine = gcToolKit.analyze(logFile);
-        var results = machine.getAggregation(HeapOccupancyAfterCollectionSummary.class);
+        /**
+         * The JavaVirtualMachine contains the aggregations as filled out by the Aggregators.
+         * It also contains configuration information about how the JVM was configured for the runtime.
+         */
+        JavaVirtualMachine machine = gcToolKit.analyze(logFile);
+
+        // Retrieves the Aggregation for HeapOccupancyAfterCollectionSummary. This is a time-series aggregation.
+        Optional<HeapOccupancyAfterCollectionSummary> results = machine.getAggregation(HeapOccupancyAfterCollectionSummary.class);
 
         if (results.isPresent()) {
-            var aggregationResult = results.get();
-            aggregationResult.get().forEach((gcType, dataSet) -> System.out
-                    .println("The XYDataSet for " + gcType + " contains " + dataSet.size() + " items."));
+            // Simple treatment of results
+            HeapOccupancyAfterCollectionSummary aggregationResult = results.get();
+            String message = "The XYDataSet for %s contains %s items.\n";
+            aggregationResult.get().forEach((gcType, dataSet) -> System.out.printf(message, gcType, dataSet.size()));
         } else {
             System.out.println("No aggregation found.");
         }
