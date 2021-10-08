bfdfca77b2ff276b440e6d963dbd52a4508b3a98
David Grieve <david.grieve@microsoft.com>
2021-10-08 12:14:48 -0400

    fix DateTimeStampTest - new methods added need to be public

--- api/src/test/java/com/microsoft/gctoolkit/time/DateTimeStampTest.java
+++ api/src/test/java/com/microsoft/gctoolkit/time/DateTimeStampTest.java
@@ -498,7 +498,7 @@ public class DateTimeStampTest {
     }
 
     @Test
-    void compareWithNullDates() {
+    public void compareWithNullDates() {
         DateTimeStamp stamp1 = new DateTimeStamp((String)null, 100);
         DateTimeStamp stamp2 = new DateTimeStamp((String)null, 200);
         DateTimeStamp stamp3 = new DateTimeStamp((String)null, 100);
@@ -508,7 +508,7 @@ public class DateTimeStampTest {
     }
 
     @Test
-    void compareToTransitivity() {
+    public void compareToTransitivity() {
         DateTimeStamp stamp1 = new DateTimeStamp("2021-09-01T11:12:13.111-0100", 100);
         DateTimeStamp stamp2 = new DateTimeStamp((String)null, 200);
         DateTimeStamp stamp3 = new DateTimeStamp("2021-08-31T11:12:13.111-0100", 300);
