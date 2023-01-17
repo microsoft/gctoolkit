30240a808bfc0022296d74b3a09fb093673e7104
Kirk Pepperdine <kcp@microsoft.com>
2023-01-16 21:56:10 -0800

    tidy: code cleanup after refactoring

--- vertx/src/test/java/com/microsoft/gctoolkit/vertx/io/GarbageCollectionEventSourceTest.java
+++ vertx/src/test/java/com/microsoft/gctoolkit/vertx/io/GarbageCollectionEventSourceTest.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.concurrent.CountDownLatch;
-import java.util.function.Consumer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -88,11 +87,6 @@ public class GarbageCollectionEventSourceTest {
         GCLogConsumer consumer = new GCLogConsumer();
         VertxDataSourceChannel channel = new VertxDataSourceChannel();
         channel.registerListener(consumer);
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         long[] observedNumberOfLines = {0L};
         try {
             logFile.stream().forEach(message -> {
