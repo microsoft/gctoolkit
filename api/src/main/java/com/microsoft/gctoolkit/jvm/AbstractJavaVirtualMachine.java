ba62592f500ae6d26ed5025ac59154fb59965571
Martijn Verburg <martijnverburg@gmail.com>
2023-01-31 20:07:48 +0000

    Update api/src/main/java/com/microsoft/gctoolkit/jvm/AbstractJavaVirtualMachine.java

--- api/src/main/java/com/microsoft/gctoolkit/jvm/AbstractJavaVirtualMachine.java
+++ api/src/main/java/com/microsoft/gctoolkit/jvm/AbstractJavaVirtualMachine.java
@@ -200,7 +200,7 @@ public abstract class AbstractJavaVirtualMachine implements JavaVirtualMachine {
             } else {
                 LOGGER.log(Level.INFO, "No Aggregations have been registered, DataSource will not be analysed.");
                 LOGGER.log(Level.INFO, "Is there a module containing Aggregation classes on the module-path");
-                LOGGER.log(Level.INFO,"Is GCToolKit::loadAggregationsFromServiceLoader() or GCToolKit::loadAggregation(Aggregation) being invoked?");
+                LOGGER.log(Level.INFO, "Is GCToolKit::loadAggregationsFromServiceLoader() or GCToolKit::loadAggregation(Aggregation) being invoked?");
             }
             dataSourceBus.close();
             eventBus.close();
