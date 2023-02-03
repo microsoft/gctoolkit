375ef0a272f48bfa5d0ddaf4ad3e7ebe727a5bf1
Kirk Pepperdine <kcp@microsoft.com>
2023-02-03 06:33:34 -0800

    refactor: clean up after git weirdness

--- api/src/main/java/com/microsoft/gctoolkit/jvm/AbstractJavaVirtualMachine.java
+++ api/src/main/java/com/microsoft/gctoolkit/jvm/AbstractJavaVirtualMachine.java
@@ -200,15 +200,7 @@ public abstract class AbstractJavaVirtualMachine implements JavaVirtualMachine {
             } else {
                 LOGGER.log(Level.INFO, "No Aggregations have been registered, DataSource will not be analysed.");
                 LOGGER.log(Level.INFO, "Is there a module containing Aggregation classes on the module-path");
-<<<<<<< HEAD
-<<<<<<< HEAD
                 LOGGER.log(Level.INFO, "Is GCToolKit::loadAggregationsFromServiceLoader() or GCToolKit::loadAggregation(Aggregation) being invoked?");
-=======
-                LOGGER.log(Level.INFO,"Is GCToolKit::loadAggregationsFromServiceLoader() or GCToolKit::loadAggregation(Aggregation) being invoked?");
->>>>>>> b92789f (bug: Add in code and logging to guard against no Aggergation classes being registered. Issue #265)
-=======
-                LOGGER.log(Level.INFO, "Is GCToolKit::loadAggregationsFromServiceLoader() or GCToolKit::loadAggregation(Aggregation) being invoked?");
->>>>>>> ba62592 (Update api/src/main/java/com/microsoft/gctoolkit/jvm/AbstractJavaVirtualMachine.java)
             }
             dataSourceBus.close();
             eventBus.close();
