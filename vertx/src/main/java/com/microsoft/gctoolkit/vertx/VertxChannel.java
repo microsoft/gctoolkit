14f5be47cc02a4457b202accc6f852246b639140
David Grieve <dagrieve@microsoft.com>
2023-04-18 14:32:36 -0400

    Make sure Vertx is closed

--- vertx/src/main/java/com/microsoft/gctoolkit/vertx/VertxChannel.java
+++ vertx/src/main/java/com/microsoft/gctoolkit/vertx/VertxChannel.java
@@ -4,11 +4,16 @@ import com.microsoft.gctoolkit.event.jvm.JVMEvent;
 import com.microsoft.gctoolkit.vertx.io.JVMEventCodec;
 import io.vertx.core.Vertx;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class VertxChannel {
 
-    private Vertx vertx;
+    protected static final Logger LOGGER = Logger.getLogger(VertxChannel.class.getName());
 
-    {
+    private static final Vertx vertx;
+
+    static {
         //Disable unused Vert.x functionality
         System.setProperty("vertx.disableFileCPResolving", "true");
         System.setProperty("vertx.disableFileCaching", "true");
@@ -21,6 +26,16 @@ public class VertxChannel {
 
     protected Vertx vertx() {
         return vertx;
+    }
+
+    public void close() {
+        vertx().close(result -> {
+            if (result.succeeded()) {
+                LOGGER.log(Level.FINE, "Vertx: closed");
+            } else {
+                LOGGER.log(Level.FINE, "Vertx: close failed", result.cause());
+            }
+        });
     }
 
 }
