21470e025b82f4075492ffd3a3c22b2a30d995a8
David Grieve <dagrieve@microsoft.com>
2024-11-20 11:04:32 -0500

    cleanup github-release script

--- .devops/scripts/github-release.sh
+++ .devops/scripts/github-release.sh
@@ -7,8 +7,6 @@ PROJECT_HOME=$SCRIPT_DIR/../..
 
 cd $PROJECT_HOME
 
-branch=$( git branch --show-current )
-
 previous_release_version=$( \
   curl --location --silent \
   -H "Accept: application/vnd.github+json" \
@@ -16,16 +14,9 @@ previous_release_version=$( \
 )
 previous_release_version=$( perl -pe 's/"tag_name":\s+"(.*?)",?/$1/' <<< $previous_release_version )
 
-#release_version=$( git log --grep "\[maven-release-plugin\] prepare release" --pretty="%h" | head -n 1 )
 release_version=$( git tag --sort=-taggerdate --list | head -n 1 )
 
-git checkout -d ${release_version}
-
-# Don't exit if the jreleaser task fails so
-# we can get back to our previous branch.
-set +e
 ./mvnw -B -pl :gctoolkit -Pjreleaser jreleaser:release \
   -Djreleaser.previous.tag.name=${previous_release_version} \
   -Djreleaser.tag.name=${release_version}
 
-git checkout ${branch}
