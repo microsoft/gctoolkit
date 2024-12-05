8a820c42522a9155afb2377a10c1730e9688e32f
David Grieve <dagrieve@microsoft.com>
2024-12-05 13:20:12 -0500

    minimal javadoc for vertx and parser to satisfy release task

--- parser/src/main/java/com/microsoft/gctoolkit/parser/GCLogParser.java
+++ parser/src/main/java/com/microsoft/gctoolkit/parser/GCLogParser.java
@@ -18,24 +18,36 @@ import com.microsoft.gctoolkit.time.DateTimeStamp;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Abstract class representing a parser for GC log files.
+ * Implements the DataSourceParser and SharedPatterns interfaces.
+ */
 public abstract class GCLogParser implements DataSourceParser, SharedPatterns {
 
     private static final Logger LOGGER = Logger.getLogger(GCLogParser.class.getName());
+    /**
+     * Special string to indicate the end of the data in the GC log.
+     */
     public static final String END_OF_DATA_SENTINEL = GCLogFile.END_OF_DATA_SENTINEL;
 
+    // TODO: GCID_COUNTER should be in SharedPatterns, not here.
+    /**
+     * Rule for parsing the GCID counter.
+     */
     public static final GCParseRule GCID_COUNTER = new GCParseRule("GCID_COUNTER", " GC\\((\\d+)\\) ");
     private JVMEventChannel consumer;
     protected Diary diary;
     private DateTimeStamp clock = new DateTimeStamp(DateTimeStamp.EPOC, 0.0d);
     private double lastDuration = 0.0d;
 
-
+    /**
+     * Default constructor.
+     */
     public GCLogParser() {}
 
     /**
-     * Setting the diary will set the current clock time to the time of the first event
-     * in the GC log. This is a better choice than EPOC though EPOC is better than null.
-     * @param diary summary the GC log.
+     * Sets the diary and initializes the clock to the time of the first event in the GC log.
+     * @param diary summary of the GC log.
      */
     @Override
     public void diary(Diary diary) {
@@ -43,53 +55,67 @@ public abstract class GCLogParser implements DataSourceParser, SharedPatterns {
         this.clock = diary.getTimeOfFirstEvent();
     }
 
+    /**
+     * Gets the current clock time.
+     * @return the current DateTimeStamp.
+     */
     public DateTimeStamp getClock() {
         return clock;
     }
 
+    /**
+     * Sets the clock to a new value.
+     * @param newValue the new DateTimeStamp value.
+     */
     public void setClock(DateTimeStamp newValue) {
         this.clock = newValue;
     }
 
+    /**
+     * Abstract method to get the name of the parser.
+     * @return the name of the parser.
+     */
     public abstract String getName();
 
+    /**
+     * Abstract method to process a trace line from the GC log.
+     * @param trace the trace line to process.
+     */
     protected abstract void process(String trace);
 
+    /**
+     * Abstract method to advance the clock based on a record.
+     * @param record the record to use for advancing the clock.
+     */
     abstract void advanceClock(String record);
 
     /**
-     * The assumption is, this method will manage the global clock and thus should never be over ridden
-     * @param now - DateTimeStamp from the current GC log record
+     * Advances the clock to the specified time.
+     * @param now the new DateTimeStamp.
      */
     protected final void advanceClock(DateTimeStamp now) {
         if (now == null)
             return;
-        // now can be the same but it can't be less than
         else if (now.before(getClock())) {
             LOGGER.log(Level.WARNING, "Log File may be Corrupted: Time traveled backwards from {0} to {1}", new Object[]{getClock().toString(), now.toString()});
         }
-        //todo: should preserve date in cases where statements do not respect PrintDateStamp (eg. ergonomics)
         setClock(now);
     }
 
     /**
-     * The clock is advanced to the time at the end of the event.
-     * The times reported in the GCLog file are "noisy" at best. This
-     * hyperactives the clock rollback detection which doesn't allow
-     * for slop. To minimize this, the clock only records when a time
-     * stamp is presented and isn't advanced to the end of the event
-     * except in the case of EOF where the time of JVM termination has
-     * not been recorded. In this case, JVMTermination time should advanced
-     * to after the previous event has ended.
-     *
-     * @param channel to publish to
-     * @param event   to be published
+     * Publishes a JVM event to the specified channel.
+     * @param channel the channel to publish to.
+     * @param event the event to be published.
      */
     public void publish(ChannelName channel, JVMEvent event) {
         lastDuration = event.getDuration();
-        consumer.publish(channel,event);
+        consumer.publish(channel, event);
     }
 
+    /**
+     * Receives a trace line and processes it.
+     * @param trace the trace line to process.
+     */
     public void receive(String trace) {
         if (!trace.equals(END_OF_DATA_SENTINEL))
             advanceClock(trace);
@@ -98,65 +124,85 @@ public abstract class GCLogParser implements DataSourceParser, SharedPatterns {
         process(trace);
     }
 
+    /**
+     * Checks if the diary indicates a pre-JDK 1.7.0_40 version.
+     * @return true if pre-JDK 1.7.0_40, false otherwise.
+     */
     boolean isPreJDK17040() {
         return diary.isPre70_40();
     }
 
+    /**
+     * Checks if the diary has PrintGCDetails enabled.
+     * @return true if PrintGCDetails is enabled, false otherwise.
+     */
     boolean hasPrintGCDetails() {
         return diary.isPrintGCDetails();
     }
 
-    // todo: mixes aggregator with parsing. premature optimization...
+    /**
+     * Extracts a MemoryPoolSummary from a GCLogTrace.
+     * @param trace the GCLogTrace to extract from.
+     * @param offset the offset to use.
+     * @return the extracted MemoryPoolSummary.
+     */
     MemoryPoolSummary getTotalOccupancyBeforeAfterWithTotalHeapPoolSizeSummary(GCLogTrace trace, int offset) {
         return trace.getOccupancyBeforeAfterWithMemoryPoolSizeSummary(offset);
     }
 
+    /**
+     * Extracts a MemoryPoolSummary from a GCLogTrace.
+     * @param trace the GCLogTrace to extract from.
+     * @param offset the offset to use.
+     * @return the extracted MemoryPoolSummary.
+     */
     MemoryPoolSummary getTotalOccupancyWithTotalHeapSizeSummary(GCLogTrace trace, int offset) {
         return trace.getOccupancyWithMemoryPoolSizeSummary(offset);
     }
 
+    /**
+     * Extracts a GCLogTrace from a line using a specified GCParseRule.
+     * @param line the line to parse.
+     * @param rule the GCParseRule to use.
+     * @return the extracted GCLogTrace.
+     */
     GCLogTrace extractReferenceBlock(String line, GCParseRule rule) {
         return rule.parse(line);
     }
 
+    /**
+     * Extracts a ReferenceGCSummary from a line.
+     * @param line the line to parse.
+     * @return the extracted ReferenceGCSummary.
+     */
     ReferenceGCSummary extractPrintReferenceGC(String line) {
-
         ReferenceGCSummary summary = new ReferenceGCSummary();
         GCLogTrace trace;
         if ((trace = extractReferenceBlock(line, SOFT_REFERENCE)) != null)
             summary.addSoftReferences(trace.getDateTimeStamp(), trace.getIntegerGroup(6), trace.getDuration());
         if ((trace = extractReferenceBlock(line, WEAK_REFERENCE)) != null)
             summary.addWeakReferences(trace.getDateTimeStamp(), trace.getIntegerGroup(6), trace.getDuration());
-
         if ((trace = extractReferenceBlock(line, FINAL_REFERENCE)) != null)
             summary.addFinalReferences(trace.getDateTimeStamp(), trace.getIntegerGroup(6), trace.getDuration());
-
         if ((trace = extractReferenceBlock(line, PHANTOM_REFERENCE)) != null) {
             if (trace.groupNotNull(7)) {
                 summary.addPhantomReferences(trace.getDateTimeStamp(), trace.getIntegerGroup(6), trace.getIntegerGroup(7), trace.getDuration());
-            }
-            else
+            } else
                 summary.addPhantomReferences(trace.getDateTimeStamp(), trace.getIntegerGroup(6), trace.getDuration());
         }
-
         if ((trace = extractReferenceBlock(line, JNI_REFERENCE)) != null) {
             if (trace.groupNotNull(6))
                 summary.addJNIWeakReferences(trace.getDateTimeStamp(), trace.getIntegerGroup(6), trace.getDuration());
             else
                 summary.addJNIWeakReferences(trace.getDateTimeStamp(), trace.getDuration());
         }
-
         return summary;
-
     }
 
-    /*
-     * [PSPermGen: 7034K->7034K(21248K)]
-     * [CMS Perm : 10696K->10696K(21248K)]
-     * [Perm : 16382K->16382K(16384K)]
-     * [Perm : 16382K(16384K)]
-     *
-     * @param line from GC log file
+    /**
+     * Extracts a MemoryPoolSummary from a line representing PermGen or Metaspace records.
+     * @param line the line to parse.
+     * @return the extracted MemoryPoolSummary.
      */
     MemoryPoolSummary extractPermOrMetaspaceRecord(String line) {
         GCLogTrace trace;
@@ -182,20 +228,34 @@ public abstract class GCLogParser implements DataSourceParser, SharedPatterns {
             int index = (trace.getGroup(1) == null) ? 1 : 3;
             metaDataPool = new MetaspaceRecord(trace.toKBytes(index), trace.toKBytes(3), trace.toKBytes(5));
         }
-
         return metaDataPool;
     }
 
+    /**
+     * Extracts a PermGenSummary from a GCLogTrace.
+     * @param trace the GCLogTrace to extract from.
+     * @return the extracted PermGenSummary.
+     */
     MemoryPoolSummary extractPermGenRecord(GCLogTrace trace) {
         int index = (trace.getGroup(2) == null) ? 2 : 4;
         return new PermGenSummary(trace.getLongGroup(index), trace.getLongGroup(4), trace.getLongGroup(6));
     }
 
+    /**
+     * Extracts the GCID from a line.
+     * @param line the line to parse.
+     * @return the extracted GCID, or -1 if not found.
+     */
     int extractGCID(String line) {
         GCLogTrace trace = GCID_COUNTER.parse(line);
         return (trace != null) ? trace.getIntegerGroup(1) : -1;
     }
 
+    /**
+     * Extracts a CPUSummary from a line.
+     * @param line the line to parse.
+     * @return the extracted CPUSummary, or null if not found.
+     */
     CPUSummary extractCPUSummary(String line) {
         GCLogTrace trace;
         if ((trace = CPU_BREAKDOWN.parse(line)) != null) {
@@ -204,11 +264,19 @@ public abstract class GCLogParser implements DataSourceParser, SharedPatterns {
         return null;
     }
 
+    /**
+     * Sets the JVMEventChannel to publish to.
+     * @param channel the channel to publish to.
+     */
     @Override
     public void publishTo(JVMEventChannel channel) {
         this.consumer = channel;
     }
 
+    /**
+     * Gets the channel name.
+     * @return the channel name.
+     */
     @Override
     public ChannelName channel() {
         return ChannelName.DATA_SOURCE;
--- vertx/src/main/java/com/microsoft/gctoolkit/vertx/VertxChannel.java
+++ vertx/src/main/java/com/microsoft/gctoolkit/vertx/VertxChannel.java
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 package com.microsoft.gctoolkit.vertx;
 
 import com.microsoft.gctoolkit.event.jvm.JVMEvent;
@@ -7,31 +9,43 @@ import io.vertx.core.Vertx;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * A class that represents a Vert.x channel for handling JVM events.
+ */
 public class VertxChannel {
 
+    // Logger for the VertxChannel class.
     protected static final Logger LOGGER = Logger.getLogger(VertxChannel.class.getName());
 
-    //
     // Note well! This cannot be a static final field.
     // UnifiedJavaVirtualMachineConfigurationTest hangs if it is.
-    //
     private final Vertx vertx;
 
     {
-        //Disable unused Vert.x functionality
+        // Disable unused Vert.x functionality
         System.setProperty("vertx.disableFileCPResolving", "true");
         System.setProperty("vertx.disableFileCaching", "true");
         vertx = Vertx.vertx();
         vertx.eventBus().registerDefaultCodec(JVMEvent.class, new JVMEventCodec());
     }
 
+    /**
+     * Default constructor.
+     */
     protected VertxChannel() {
     }
 
+    /**
+     * Gets the Vert.x instance.
+     * @return the Vert.x instance.
+     */
     protected Vertx vertx() {
         return vertx;
     }
 
+    /**
+     * Closes the Vert.x instance.
+     */
     public void close() {
         vertx().close(result -> {
             if (result.succeeded()) {
@@ -41,5 +55,4 @@ public class VertxChannel {
             }
         });
     }
-
 }
