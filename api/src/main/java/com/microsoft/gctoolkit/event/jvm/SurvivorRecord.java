12e7e2b62c47eff83d7683ecd3e3e682ea246192
Kirk Pepperdine <kcp@microsoft.com>
2025-04-21 19:02:14 -0700

    refactor: add in tests for Survivor records found in GCEvent

--- api/src/main/java/com/microsoft/gctoolkit/event/jvm/SurvivorRecord.java
+++ api/src/main/java/com/microsoft/gctoolkit/event/jvm/SurvivorRecord.java
@@ -17,7 +17,7 @@ public class SurvivorRecord extends JVMEvent {
 
     // JDK bug, we have now seen a max tenuring threshold of 32, even 64
     // Fold anything older than 15 back into the 15th slot
-    private ArrayList<Long> bytesAtAge = null;
+    private long[] bytesAtAge;
 
     public SurvivorRecord(DateTimeStamp timeStamp, long desiredOccupancy, int calculatedTenuringThreshold, int maxTenuringThreshold) {
         super(timeStamp, 0.0d);
@@ -56,7 +56,7 @@ public class SurvivorRecord extends JVMEvent {
 
     public long getBytesAtAge(int age) {
         if (this.bytesAtAge == null) return 0L;
-        return this.bytesAtAge.get(age);
+        return this.bytesAtAge[age];
     }
 
     /*
@@ -67,20 +67,20 @@ public class SurvivorRecord extends JVMEvent {
     public void add(int age, long bytes) {
 
         if (bytesAtAge == null) {
-            bytesAtAge = new ArrayList<>();
-            bytesAtAge.add(0L); //throw away the first slow.
+            bytesAtAge = new long[maxTenuringThreshold+1];
+            bytesAtAge[0] = 0L; //throw away the first slot.
         }
 
         if (age <= maxTenuringThreshold) {
-            bytesAtAge.add(bytes);
+            bytesAtAge[age] = bytes;
         } else {
-            bytesAtAge.set(maxTenuringThreshold, bytesAtAge.get(maxTenuringThreshold) + bytes);
+            bytesAtAge[age] += bytes;
         }
     }
 
-    public Long[] getBytesAtEachAge() {
+    public long[] getBytesAtEachAge() {
         if (bytesAtAge == null)
-            return new Long[0];
-        return bytesAtAge.toArray(new Long[0]);
+            return new long[0];
+        return bytesAtAge;
     }
 }
