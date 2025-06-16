fb58a04c0ad98d62f0f859814d0bb09bd95f31a4
Anthony Kiselev <Anthony.Kiselev@imc.com>
2025-06-16 13:48:53 -0500

    Fix inconsistent whitespace

--- api/src/main/java/com/microsoft/gctoolkit/jvm/SupportedFlags.java
+++ api/src/main/java/com/microsoft/gctoolkit/jvm/SupportedFlags.java
@@ -37,7 +37,7 @@ public enum SupportedFlags {
     PRINT_PROMOTION_FAILURE,                    // 26
     PRINT_FLS_STATISTICS,                       // 27
     
-    PRINT_CPU_TIMES,							// 28
+    PRINT_CPU_TIMES,                            // 28
 
     GENERATIONAL_ZGC,                           // 29
 
