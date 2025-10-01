bf7640a659d8b36d96c8693809fe13dfd9e76fc4
chris-chedgey-sonarsource <chris.chedgey@sonarsource.com>
2025-10-01 12:51:16 +0100

    Update SharedAggregation.java

--- IT/src/main/java/com/microsoft/gctoolkit/integration/shared/SharedAggregation.java
+++ IT/src/main/java/com/microsoft/gctoolkit/integration/shared/SharedAggregation.java
@@ -1,11 +1,12 @@
 package com.microsoft.gctoolkit.integration.shared;
 
 import com.microsoft.gctoolkit.aggregator.Aggregation;
+import com.microsoft.gctoolkit.parser;
 
 public abstract class SharedAggregation extends Aggregation {
 
     public double getRuntimeDuration() { return super.estimatedRuntime(); }
     // added for testing
-    public TwoRuntimeReport report;
+    public UnifiedG1GCParser testonly;
 
 }
