d5b5b93225130ea5657f3e27dc82fec5f420950b
chris-chedgey-sonarsource <chris.chedgey@sonarsource.com>
2025-10-02 15:48:45 +0100

    Update SharedAggregation.java to (temp) remove deviating reference

--- IT/src/main/java/com/microsoft/gctoolkit/integration/shared/SharedAggregation.java
+++ IT/src/main/java/com/microsoft/gctoolkit/integration/shared/SharedAggregation.java
@@ -2,12 +2,12 @@ package com.microsoft.gctoolkit.integration.shared;
 
 import com.microsoft.gctoolkit.aggregator.Aggregation;
 // added for testing
-import com.microsoft.gctoolkit.parser.UnifiedG1GCParser;
+//import com.microsoft.gctoolkit.parser.UnifiedG1GCParser;
 
 public abstract class SharedAggregation extends Aggregation {
 
     public double getRuntimeDuration() { return super.estimatedRuntime(); }
     // added for testing
-    public UnifiedG1GCParser testonly;
+    //public UnifiedG1GCParser testonly;
 
 }
