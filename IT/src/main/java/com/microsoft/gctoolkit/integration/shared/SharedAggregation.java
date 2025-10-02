44c6ecb5fa1565bbabeb45ef6baa6946c3485471
chris-chedgey-sonarsource <chris.chedgey@sonarsource.com>
2025-10-02 14:26:23 +0100

    Update SharedAggregation.java

--- IT/src/main/java/com/microsoft/gctoolkit/integration/shared/SharedAggregation.java
+++ IT/src/main/java/com/microsoft/gctoolkit/integration/shared/SharedAggregation.java
@@ -1,6 +1,7 @@
 package com.microsoft.gctoolkit.integration.shared;
 
 import com.microsoft.gctoolkit.aggregator.Aggregation;
+// added for testing
 import com.microsoft.gctoolkit.parser.UnifiedG1GCParser;
 
 public abstract class SharedAggregation extends Aggregation {
